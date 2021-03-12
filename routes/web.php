3258952439fcd1c16c513dc8e45e7f3e84c797dd
Carlos Henrique Rubia Barbosa <henriquerubiab@gmail.com>
2021-03-11 23:59:05 -0300

    Feat: permitir criação e exibição de postagens

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Http\Controllers\PostsController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', [PostsController::class, 'index']);
